1b65b9d6732701b3a75598b3ac9215f9a7ea0c90
Didiel <dfigueroa@ikatechsolutions.com>
2022-01-08 00:36:57 -0300

    version 1.0.0-rc lista

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,10 +5,10 @@ import { ProductContextProps, Product, onChangeArgs, InitialValues, ProductCardH
 
 import styles from '../styles/styles.module.css'
 
+
+
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
-
-
 
 export interface Props {
     product: Product;
@@ -21,7 +21,6 @@ export interface Props {
     initialValues?: InitialValues;
     maxCount?: number;
 }
-
 
 export const ProductCard = ({ children, product, className, style, onChange, value, initialValues, maxCount }: Props ) => {
 
